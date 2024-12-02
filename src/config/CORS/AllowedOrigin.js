308d245c9e0738200f4475d4fe9783cf326510bb
bached <thebobz2770@gmail.com>
2024-12-02 08:25:38 +0700

    grant CORS permission

--- src/config/CORS/AllowedOrigin.js
+++ src/config/CORS/AllowedOrigin.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     '*',
     'http://localhost:3000',
     'http://localhost:8080',
-    'https://card-tcg.vercel.app'
+    'https://card-tcg.vercel.app',
+    'https://tcg-collection.onrender.com'
 ];
 
 module.exports = allowedOrigins;
